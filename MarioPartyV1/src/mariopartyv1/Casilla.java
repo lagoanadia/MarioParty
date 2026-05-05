2455661133f4862c0babd6e77ffd9f1ea9c11cd8
Nadia <lagoanadia@gmail.com>
2026-05-05 08:53:44 +0200

    Refactor Casilla class to support multiple successors

--- MarioPartyV1/src/mariopartyv1/Casilla.java
+++ MarioPartyV1/src/mariopartyv1/Casilla.java
@@ -1,8 +1,10 @@
 package mariopartyv1;
 
-public class Casilla 
+import java.util.ArrayList;
+
+public abstract class Casilla 
 {
-    private Casilla siguiente;
+    private ArrayList<Casilla> siguientes= new ArrayList<>(); // varias opciones
     private String nombre;
 
     public Casilla(String nombre)
@@ -17,15 +19,19 @@ public class Casilla
 
     public void setSiguiente(Casilla c)
     {
-        this.siguiente = c;
+       siguientes.add(c);
+    }
+    public ArrayList<Casilla> getSiguienteList()
+    {
+        return siguientes;
     }
 
     public Casilla getSiguiente()
     {
-        return siguiente;
+     
+        //por ahora no implemento bifurcaciones pero dejo el arraylist listo.
+        return siguientes.get(0);
     }
-    public void accion()
-    {
-        
-    }
+    public abstract void accion(Jugador j);
+   
 }
