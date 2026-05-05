04cf3428ce7bc7e38dfcf4ae8e9e9523a2780e3c
Nadia <lagoanadia@gmail.com>
2026-05-05 08:54:46 +0200

    Add action method to CasillaAzul class
    Implemented action method for CasillaAzul to award coins.

--- MarioPartyV1/src/mariopartyv1/CasillaAzul.java
+++ MarioPartyV1/src/mariopartyv1/CasillaAzul.java
@@ -1,17 +1,18 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package mariopartyv1;
 
-/**
- *
- * @author Nadia
- */
 public class CasillaAzul extends Casilla {
     
     public CasillaAzul(String nombre) {
         super(nombre);
+    }
+
+    @Override
+    public void accion(Jugador j) 
+    {
+        System.out.println(j.getNombre()+" que afortunado! ganas 3 monedas. Yay!");
+        j.alterMonedas(3);
+        System.out.println("monedas:"+j.getMonedas());
     }
     
   
